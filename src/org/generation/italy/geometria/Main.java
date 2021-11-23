b4eb2cb14245510fbc8887d59c5e815cee71100b
AndreaConti <andrea.conti23331@gmail.com>
2021-11-23 16:19:54 +0100

    Correzione

--- src/org/generation/italy/geometria/Main.java
+++ src/org/generation/italy/geometria/Main.java
@@ -1,15 +1,26 @@
 package org.generation.italy.geometria;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
 
-		Rettangolo mioRettangolo = new Rettangolo();
+		System.out.print("Inserire valore in centimetri per la base del rettangolo: ");
+		int base = scanner.nextInt();
 
-		mioRettangolo.baseUtente();
-		mioRettangolo.altezzaUtente();
+		System.out.print("Inserire un valore in centimetri per l'altezza del rettangolo: ");
+		int altezza = scanner.nextInt();
+
+		Rettangolo mioRettangolo = new Rettangolo(base, altezza);
+
 		mioRettangolo.calcoloArea();
+		System.out.println("L'area del tuo rettangolo è di: " + mioRettangolo.calcoloArea() + " centimetri quadrati.");
 		mioRettangolo.CalcoloPerimetro();
+		System.out.println("Il perimetro del tuo rettangolo è di: " + mioRettangolo.CalcoloPerimetro() + " centimetri.");
+
+		scanner.close();
 	}
 
 }
--- src/org/generation/italy/geometria/Rettangolo.java
+++ src/org/generation/italy/geometria/Rettangolo.java
@@ -1,37 +1,25 @@
 package org.generation.italy.geometria;
 
-import java.util.Scanner;
-
 public class Rettangolo {
-	Scanner scanner = new Scanner(System.in);
 
 	// Variabili
 	int base;
 	int altezza;
 
-	// Metodo richiesta "base" all'utente
-	void baseUtente() {
-		System.out.print("Inserire valore in centimetri per la base del rettangolo: ");
-		base = scanner.nextInt();
-	}
-
-	// Metodo richiesta "altezza" all'utente
-	void altezzaUtente() {
-		System.out.print("Inserire un valore in centimetri per l'altezza del rettangolo: ");
-		altezza = scanner.nextInt();
+	Rettangolo(int base, int altezza) {
+		this.base = base;
+		this.altezza = altezza;
 	}
 
 	// Metodo per calcolo area
 	int calcoloArea() {
 		int area = base * altezza;
-		System.out.println("L'area del tuo rettangolo è di: " + area + " centimetri quadrati.");
 		return area;
 	}
 
 	// Metodo per calcolo perimetro
 	int CalcoloPerimetro() {
 		int perimetro = (base * 2) + (altezza * 2);
-		System.out.println("Il perimetro del tuo rettangolo è di: " + perimetro + " centimetri.");
 		return perimetro;
 	}
 }
