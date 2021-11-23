383f060dbc58bf7ac079985183c83c391419d6e4
AndreaConti <andrea.conti23331@gmail.com>
2021-11-23 15:49:22 +0100

    Consegna

---
+++ src/org/generation/italy/geometria/Main.java
@@ -0,0 +1,15 @@
+package org.generation.italy.geometria;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Rettangolo mioRettangolo = new Rettangolo();
+
+		mioRettangolo.baseUtente();
+		mioRettangolo.altezzaUtente();
+		mioRettangolo.calcoloArea();
+		mioRettangolo.CalcoloPerimetro();
+	}
+
+}
